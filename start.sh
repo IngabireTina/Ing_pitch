583348d5a08721cec13daac2c4bc731cad198013
IngabireTina <ingabire067@gmail.com>
2021-04-05 15:54:30 -0400

    corrected errors

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://tina:Ingabire067@localhost/pitchess'
-export MAIL_USERNAME='ingabire067@gmail.com'
+export MAIL_USERNAME='ithinah07@gmail.com'
 export MAIL_PASSWORD='ingabire'
 export SECRET_KEY='ingabire'
 
